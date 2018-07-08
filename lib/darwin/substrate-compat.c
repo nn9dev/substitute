ae12429a3401c1bda41001481bb8d0808ba122f1
Thatchapon Unprasert <poomst@hotmail.com>
2018-07-08 10:49:26 -0700

    Cydia Substrate wrapper.

--- lib/darwin/substrate-compat.c
+++ lib/darwin/substrate-compat.c
@@ -64,3 +64,53 @@ void SubHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result) {
                          substitute_strerror(ret));
     }
 }
+
+EXPORT
+void *MSGetImageByName(const char *filename) {
+    return SubGetImageByName(filename);
+}
+
+EXPORT
+void *MSFindSymbol(void *image, const char *name) {
+	return SubFindSymbol(image, name);
+}
+
+EXPORT
+void MSHookFunction(void *symbol, void *replace, void **result) {
+	SubHookFunction(symbol, replace, result);
+}
+
+EXPORT
+void MSHookMessageEx(Class _class, SEL sel, IMP imp, IMP *result) {
+	if (class_getInstanceMethod(_class, sel) || class_getClassMethod(_class, sel)) {
+		SubHookMessageEx(_class, sel, imp, result);
+	} else {
+		substitute_panic("libsubstrate-shim: Tried to hook non-existent selector %s on class %s",
+			sel_getName(sel), class_getName(_class));
+			if (result) *result = NULL;
+	}
+}
+
+// i don't think anyone uses this function anymore, but it's here for completeness
+EXPORT
+void MSHookClassPair(Class _class, Class hook, Class old) {
+    unsigned int n_methods = 0;
+    Method *hooks = class_copyMethodList(hook, &n_methods);
+    
+    for (unsigned int i = 0; i < n_methods; ++i) {
+        SEL selector = method_getName(hooks[i]);
+        const char *what = method_getTypeEncoding(hooks[i]);
+        
+        Method old_mptr = class_getInstanceMethod(old, selector);
+        Method cls_mptr = class_getInstanceMethod(_class, selector);
+        
+        if (cls_mptr) {
+            class_addMethod(old, selector, method_getImplementation(hooks[i]), what);
+            method_exchangeImplementations(cls_mptr, old_mptr);
+        } else {
+            class_addMethod(_class, selector, method_getImplementation(hooks[i]), what);
+        }
+    }
+    
+    free(hooks);
+}
